f98c4c34f9cdde7f0338d987269b9814e20203f9
Carter Martindale <carter_martindale@college.harvard.edu>
2020-12-01 18:29:53 -0700

    Cleaned up shiny and my main R files

--- working_app/app.R
+++ working_app/app.R
@@ -13,316 +13,12 @@ library(forcats)
 library(rstanarm)
 library(gtsummary)
 library(broom.mixed)
+library(gt)
 
-d<- read_csv("SCDB_2020_01_caseCentered_LegalProvision.csv",
-             col_names = TRUE, cols(
-    caseId = col_character(),
-    docketId = col_character(),
-    caseIssuesId = col_character(),
-    voteId = col_character(),
-    dateDecision = col_character(),
-    decisionType = col_double(),
-    usCite = col_character(),
-    sctCite = col_character(),
-    ledCite = col_character(),
-    lexisCite = col_character(),
-    term = col_double(),
-    naturalCourt = col_double(),
-    chief = col_character(),
-    docket = col_character(),
-    caseName = col_character(),
-    dateArgument = col_character(),
-    dateRearg = col_character(),
-    petitioner = col_double(),
-    petitionerState = col_double(),
-    respondent = col_double(),
-    respondentState = col_double(),
-    jurisdiction = col_double(),
-    adminAction = col_double(),
-    adminActionState = col_double(),
-    threeJudgeFdc = col_double(),
-    caseOrigin = col_double(),
-    caseOriginState = col_double(),
-    caseSource = col_double(),
-    caseSourceState = col_double(),
-    lcDisagreement = col_double(),
-    certReason = col_double(),
-    lcDisposition = col_double(),
-    lcDispositionDirection = col_double(),
-    declarationUncon = col_double(),
-    caseDisposition = col_double(),
-    caseDispositionUnusual = col_double(),
-    partyWinning = col_double(),
-    precedentAlteration = col_double(),
-    voteUnclear = col_double(),
-    issue = col_double(),
-    issueArea = col_double(),
-    decisionDirection = col_double(),
-    decisionDirectionDissent = col_double(),
-    authorityDecision1 = col_double(),
-    authorityDecision2 = col_double(),
-    lawType = col_double(),
-    lawSupp = col_double(),
-    lawMinor = col_character(),
-    majOpinWriter = col_double(),
-    majOpinAssigner = col_double(),
-    splitVote = col_double(),
-    majVotes = col_double(),
-    minVotes = col_double())) %>% 
-    clean_names() %>% 
-    mutate(date_decision_new = mdy(date_decision),
-           date_argument_new = mdy(date_argument),
-           date_rearg_new = mdy(date_rearg)) %>% 
-    select(us_cite, term, chief, case_origin_state, issue,
-           issue_area, jurisdiction, decision_direction) %>% 
-    drop_na(decision_direction)
+source('D_dataset.R')
+source('Justice.R')
+source('model_1.R')
 
-justice <- read_csv("SCDB_2020_01_justiceCentered_Citation.csv", col_names = TRUE, cols(
-    caseId = col_character(),
-    docketId = col_character(),
-    caseIssuesId = col_character(),
-    voteId = col_character(),
-    dateDecision = col_character(),
-    decisionType = col_double(),
-    usCite = col_character(),
-    sctCite = col_character(),
-    ledCite = col_character(),
-    lexisCite = col_character(),
-    term = col_double(),
-    naturalCourt = col_double(),
-    chief = col_character(),
-    docket = col_character(),
-    caseName = col_character(),
-    dateArgument = col_character(),
-    dateRearg = col_character(),
-    petitioner = col_double(),
-    petitionerState = col_double(),
-    respondent = col_double(),
-    respondentState = col_double(),
-    jurisdiction = col_double(),
-    adminAction = col_double(),
-    adminActionState = col_double(),
-    threeJudgeFdc = col_double(),
-    caseOrigin = col_double(),
-    caseOriginState = col_double(),
-    caseSource = col_double(),
-    caseSourceState = col_double(),
-    lcDisagreement = col_double(),
-    certReason = col_double(),
-    lcDisposition = col_double(),
-    lcDispositionDirection = col_double(),
-    declarationUncon = col_double(),
-    caseDisposition = col_double(),
-    caseDispositionUnusual = col_double(),
-    partyWinning = col_double(),
-    precedentAlteration = col_double(),
-    voteUnclear = col_double(),
-    issue = col_double(),
-    issueArea = col_double(),
-    decisionDirection = col_double(),
-    decisionDirectionDissent = col_double(),
-    authorityDecision1 = col_double(),
-    authorityDecision2 = col_double(),
-    lawType = col_double(),
-    lawSupp = col_double(),
-    lawMinor = col_character(),
-    majOpinWriter = col_double(),
-    majOpinAssigner = col_double(),
-    splitVote = col_double(),
-    majVotes = col_double(),
-    minVotes = col_double(),
-    justice = col_double(),
-    justiceName = col_character(),
-    vote = col_double(),
-    opinion = col_double(),
-    direction = col_double(),
-    majority = col_double(),
-    firstAgreement = col_double(),
-    secondAgreement = col_double())) %>% 
-    select(voteId, term, chief,
-           caseOriginState, issueArea,
-           decisionDirection, direction, justice, justiceName, vote,
-           decisionDirectionDissent) %>% 
-    clean_names()
-
-d$issue_area[which(d$issue_area == 1)] <- "Criminal Procedure"
-d$issue_area[which(d$issue_area == 2)] <- "Civil Rights"
-d$issue_area[which(d$issue_area == 3)] <- "1st Amendment"
-d$issue_area[which(d$issue_area == 4)] <- "Due Process"
-d$issue_area[which(d$issue_area == 5)] <- "Privacy"
-d$issue_area[which(d$issue_area == 6)] <- "Attorney/Government Fees and Compensation"
-d$issue_area[which(d$issue_area == 7)] <- "Unions"
-d$issue_area[which(d$issue_area == 8)] <- "Economic Activity"
-d$issue_area[which(d$issue_area == 9)] <- "Judicial Power"
-d$issue_area[which(d$issue_area == 10)] <- "Federalism"
-d$issue_area[which(d$issue_area == 11)] <- "Interstate Relations"
-d$issue_area[which(d$issue_area == 12)] <- "Federal Taxation"
-d$issue_area[which(d$issue_area == 13)] <- "Misc"
-d$issue_area[which(d$issue_area == 14)] <- "Private Laws"
-
-justice$issue_area[which(justice$issue_area == 1)] <- "Criminal Procedure"
-justice$issue_area[which(justice$issue_area == 2)] <- "Civil Rights"
-justice$issue_area[which(justice$issue_area == 3)] <- "1st Amendment"
-justice$issue_area[which(justice$issue_area == 4)] <- "Due Process"
-justice$issue_area[which(justice$issue_area == 5)] <- "Privacy"
-justice$issue_area[which(justice$issue_area == 6)] <- "Attorney/Government Fees and Compensation"
-justice$issue_area[which(justice$issue_area == 7)] <- "Unions"
-justice$issue_area[which(justice$issue_area == 8)] <- "Economic Activity"
-justice$issue_area[which(justice$issue_area == 9)] <- "Judicial Power"
-justice$issue_area[which(justice$issue_area == 10)] <- "Federalism"
-justice$issue_area[which(justice$issue_area == 11)] <- "Interstate Relations"
-justice$issue_area[which(justice$issue_area == 12)] <- "Federal Taxation"
-justice$issue_area[which(justice$issue_area == 13)] <- "Misc"
-justice$issue_area[which(justice$issue_area == 14)] <- "Private Laws"
-
-justice$direction[which(justice$direction == 2)] <- 0
-
-d$decision_direction[which(d$decision_direction == 2)] <- 0
-
-d$jurisdiction[which(d$jurisdiction == 1)] <- "cert"
-d$jurisdiction[which(d$jurisdiction == 2)] <- "appeal"
-d$jurisdiction[which(d$jurisdiction == 3)] <- "bail"
-d$jurisdiction[which(d$jurisdiction == 4)] <- "certification"
-d$jurisdiction[which(d$jurisdiction == 5)] <- "docketing fee"
-d$jurisdiction[which(d$jurisdiction == 6)] <- "rehearsing or restored to calendar for reargument"
-d$jurisdiction[which(d$jurisdiction == 7)] <- "injunction"
-d$jurisdiction[which(d$jurisdiction == 8)] <- "mandamus"
-d$jurisdiction[which(d$jurisdiction == 9)] <- "original"
-d$jurisdiction[which(d$jurisdiction == 10)] <- "prohibition"
-d$jurisdiction[which(d$jurisdiction == 12)] <- "stay"
-d$jurisdiction[which(d$jurisdiction == 13)] <- "writ of error"
-d$jurisdiction[which(d$jurisdiction == 14)] <- "writ of habeus corpus"
-d$jurisdiction[which(d$jurisdiction == 15)] <- "unspecified"
-
-read_petition_total <- function(x, y) {
-    z <- read_excel(x, skip = 1) %>% 
-        drop_na() %>% 
-        clean_names() %>% 
-        rename("granted" = "terminated",
-               "denied" = "x5",
-               "dismissed" = "x6",
-               "nature_of_proceeding" = "circuit_and_nature_of_proceeding") %>% 
-        mutate(year = y)
-    w <- z[c(7:10, 12:15, 17:20, 22:25, 27:30, 32:35, 37:40,
-             42:45, 47:50, 52:55, 57:60, 62:65), c(1, 3:6, 8)]
-    w %>%
-        mutate(district = c(rep("DC", 4), rep("1st", 4),
-                            rep("2nd", 4), rep("3rd", 4),
-                            rep("4th", 4), rep("5th", 4),
-                            rep("6th", 4),
-                            rep("7th", 4), rep("8th", 4),
-                            rep("9th", 4), rep("10th", 4),
-                            rep("11th", 4)))
-}
-
-read_p <- function(x, y) {
-    z <- read_excel(x, skip = 9
-    ) %>%
-        drop_na() %>% 
-        clean_names() %>% 
-        rename("nature_of_proceeding" = "of_proceeding") %>% 
-        mutate(year = y)
-    w <- z[c(7:10, 12:15, 17:20, 22:25, 28:31, 33:36, 38:41,
-             43:46, 48:51, 54:57, 59:62, 64:67), c(1, 3:6, 8)]
-    w %>%
-        mutate(district = c(rep("DC", 4), rep("1st", 4),
-                            rep("2nd", 4), rep("3rd", 4),
-                            rep("4th", 4), rep("5th", 4),
-                            rep("6th", 4),
-                            rep("7th", 4), rep("8th", 4),
-                            rep("9th", 4), rep("10th", 4),
-                            rep("11th", 4)))
-}
-
-read_annoyed <- function(x, y) {
-    z <- read_excel(x, skip = 11) %>% 
-        drop_na() %>% 
-        clean_names() %>% 
-        rename("nature_of_proceeding" = "of_proceeding") %>% 
-        mutate(year = y)
-    w <- z[c(7:10, 12:15, 17:20, 22:25, 28:31, 33:36, 38:41,
-             43:46, 48:51, 54:57, 59:62, 64:67), c(1, 3:6, 8)]
-    w %>%
-        mutate(district = c(rep("DC", 4), rep("1st", 4),
-                            rep("2nd", 4), rep("3rd", 4),
-                            rep("4th", 4), rep("5th", 4),
-                            rep("6th", 4),
-                            rep("7th", 4), rep("8th", 4),
-                            rep("9th", 4), rep("10th", 4),
-                            rep("11th", 4)))
-}
-
-read_2008 <- function(x, y) {
-    z <- read_excel(x, skip = 9) %>%
-        drop_na() %>% 
-        clean_names() %>% 
-        rename("nature_of_proceeding" = "of_proceeding") %>% 
-        mutate(year = y)
-    w <- z[c(7:10, 12:15, 17:20, 22:25, 28:31, 33:36, 38:41,
-             43:46, 48:51, 53:56, 58:61, 63:66), c(1, 3:6, 8)]
-    w %>%
-        mutate(district = c(rep("DC", 4), rep("1st", 4),
-                            rep("2nd", 4), rep("3rd", 4),
-                            rep("4th", 4), rep("5th", 4),
-                            rep("6th", 4),
-                            rep("7th", 4), rep("8th", 4),
-                            rep("9th", 4), rep("10th", 4),
-                            rep("11th", 4)))
-}
-
-p_2019 <- read_petition_total("jb_b2_0930.2019.xlsx", 2019)
-p_2018 <- read_petition_total("jb_b2_0930.2018.xlsx", 2018)
-p_2017 <- read_petition_total("jb_b2_0930.2017.xlsx", 2017)
-p_2016 <- read_petition_total("jb_b2_0930.2016.xlsx", 2016)
-p_2015 <- read_p("B02Sep15.xlsx", 2015)
-p_2014 <- read_p("B02Sep14.xlsx", 2014)
-p_2013 <- read_p("B02Sep13.xlsx", 2013)
-p_2012 <- read_p("B02Sep12.xlsx", 2012)
-p_2011 <- read_p("B02Sep11.xlsx", 2011)
-p_2010 <- read_annoyed("B02Sep10.xlsx", 2010)
-p_2009 <- read_p("B02Sep09.xlsx", 2009)
-p_2008 <- read_2008("B02Sep08.xlsx", 2008)
-p_2007 <- read_p("B02Sep07.xlsx", 2007)
-p_2006 <- read_annoyed("b2_2.xlsx", 2006)
-p_2005 <- read_p("b2_1.xlsx", 2005)
-
-p_2016$nature_of_proceeding[which(p_2016$nature_of_proceeding == "U.S. Private")] <- 
-    "Private Civil"
-
-roberts_circuit <- rbind(p_2019, p_2018, p_2017, p_2016, p_2015, p_2014, p_2013, p_2012, p_2011, p_2010, p_2009, p_2008, p_2007, p_2006, p_2005)
-
-roberts_circuit$granted[which(roberts_circuit$granted ==
-                                  "-")] <- 0
-roberts_circuit$denied[which(roberts_circuit$denied ==
-                                 "-")] <- 0
-roberts_circuit$dismissed[which(roberts_circuit$dismissed ==
-                                    "-")] <- 0
-roberts_circuit$filed[which(roberts_circuit$filed ==
-                                "-")] <- 0
-
-roberts_circuit <- roberts_circuit %>%   
-    mutate(percent_granted =
-               (as.numeric(granted)/as.numeric(filed)) *100,
-           percent_denied =
-               (as.numeric(denied)/as.numeric(filed)) *100)
-
-roberts_circuit$percent_granted[which(roberts_circuit$percent_granted == "NaN")] <- 0
-roberts_circuit$percent_denied[which(roberts_circuit$percent_denied == "Inf")] <- 0
-
-fit <- stan_glm(percent_granted ~ district,
-                data = roberts_circuit,
-                refresh = 0)
-new_obs <- tibble(district = c("1st", "2nd", "3rd", "4th", "5th",
-                               "6th", "7th", "8th", "9th", "10th",
-                               "11th", "DC"))
-ep <- posterior_epred(fit,
-                      newdata = new_obs) %>% 
-    as_tibble() %>% 
-    mutate_all(as.numeric) %>% 
-    rename(DC = `12`) %>% 
-    pivot_longer(cols = 1:DC,
-                 names_to = "Circuit",
-                 values_to = "Prediction")
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(
@@ -512,23 +208,6 @@ ui <- navbarPage(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
     
-    output$plot2 <- renderPlot({
-       d %>% 
-            drop_na(input$z) %>% 
-        ggplot(aes(.data[[input$z]])) +
-       geom_bar() + 
-            labs(y = "Count",
-                 title = "Summmary of Various Topics in the Supreme Court since 1954",
-                 x = case_when(
-                     input$z == "case_origin_state" ~ "Case Origin State",
-                     input$z == "jurisdiction" ~ "Jurisdiction",
-                     input$z == "issue_area" ~ "Issue Area",
-                     input$z == "decision_direction" ~ "Decision Direction")) +
-            theme_classic() +
-            theme(axis.text.x = element_text(angle = 45,
-                                             size = 8,
-                                             color = "salmon"))
-    })
     
     output$plot3 <- renderPlot({
         d %>%
@@ -571,13 +250,14 @@ server <- function(input, output, session) {
         d %>% 
             filter(issue_area == input$b,
                    chief == input$c) %>%
+            drop_na(decision_direction) %>% 
             ggplot(aes(x = issue_area)) +
             geom_bar(fill = "deepskyblue4",
                      aes(y = (..count..)/sum(..count..))) +
             scale_y_continuous(labels = scales::percent) +
             facet_wrap(~decision_direction,
                      labeller = labeller(decision_direction = c(
-                "0" = "Liberal", "1" = "Conservative"))) +
+                "0" = "Liberal", "1" = "Conservative", "3" = "Neither"))) +
             labs(title = "Decision Direction by Issue",
                  x = "Issue Area",
                  y = "Number of Cases")
@@ -589,13 +269,27 @@ server <- function(input, output, session) {
                                        "SGBreyer", "SAAlito",
                                        "SSotomayor"),
                    issue_area == input$I)
+        
+        # This filters my data to current Justices who have been on the court
+        # for 10 or more years, and then filters it more by the issue
+        # area that is chosen by the user. 
+        
         fit_obj <- stan_glm(data = fit,
                  direction ~ justice_name - 1,
                  refresh = 0)
+        
+        # Basic model that regresses voting direction onto each individual
+        # justice. 
+        
         new_obj <- tibble(justice_name = c("JGRoberts", "CThomas",
                                            "SGBreyer", "SAAlito",
                                            "SSotomayor"))
-        ep <- posterior_epred(fit_obj, newdata = new_obj) %>% 
+        
+        # This creates the new_data I need for my posterior_epred. I decided
+        # to use epred since I didn't want any values less than one, since
+        # that would not make sense for a vote direction. 
+        
+        pp <- posterior_epred(fit_obj, newdata = new_obj) %>% 
             as_tibble() %>% 
             mutate_all(as.numeric) %>% 
             rename(Roberts = `1`,
@@ -606,8 +300,11 @@ server <- function(input, output, session) {
             pivot_longer(cols = Roberts:Sotomayor,
                          names_to = "Justice",
                          values_to = "Vote")
+        
+        # This manipulates my tibble to a form that will be easiest to work
+        # with for my ggplot.
 
-        ep %>% 
+        pp %>% 
             ggplot(aes(x = Vote)) +
             geom_histogram(aes(y = after_stat(count/sum(count)),
                                fill = Justice),
@@ -617,6 +314,12 @@ server <- function(input, output, session) {
                            color = "white") +
             geom_vline(xintercept = 0.5, lty = 2,
                        color = "red") +
+            
+            # This vline I felt made it easier for users to see the divide
+            # or the line where if the majority of a justice's data fell
+            # to the left it would be a Liberal vote, or if it was to the
+            # right it would be a conservative vote. 
+            
             labs(title = "Posterior Probability Distribution",
                  subtitle = (paste("Predicted Vote Direction on",input$I)),
                  x = "Vote Direction",
